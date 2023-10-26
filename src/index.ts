213510c97e4f6c1702885e135e3f0f59e9afa911
Imad <imadetassaoui@gmail.com>
2023-10-26 18:10:54 +0100

    ✌🏻 Setup Socket.io

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import { Server, Socket } from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(
   cors({
@@ -17,7 +19,16 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server);
+
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+});
 
 server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
